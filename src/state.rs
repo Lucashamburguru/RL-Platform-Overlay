6e1cb810566dac451d28b23a7754c8e24111ec43
LucasHamburguru <gumisfun69@gmail.com>
2026-04-30 05:07:44 -0700

    feat: add positioning and launch state to AppState

--- src/state.rs
+++ src/state.rs
@@ -3,12 +3,24 @@ use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use arc_swap::ArcSwap;
 
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub enum AnchorPos {
+    #[default]
+    TopLeft,
+    TopRight,
+    BottomLeft,
+    BottomRight,
+    CenterRight,
+}
+
 #[derive(Clone, Debug)]
 pub struct Config {
     pub transparency: u8,
     pub ui_scale: f32,
     pub show_bots: bool,
     pub window_size: [f32; 2],
+    pub anchor: AnchorPos,
+    pub monitor_index: usize,
 }
 
 impl Default for Config {
@@ -18,6 +30,8 @@ impl Default for Config {
             ui_scale: 1.0,
             show_bots: true,
             window_size: [1920.0, 1080.0],
+            anchor: AnchorPos::TopLeft,
+            monitor_index: 0,
         }
     }
 }
@@ -33,6 +47,7 @@ pub struct PlayerInfo {
 pub struct AppState {
     pub is_visible: AtomicBool,
     pub is_connected: AtomicBool,
+    pub is_launched: AtomicBool,
     pub players: ArcSwap<HashMap<String, PlayerInfo>>,
     pub config: ArcSwap<Config>,
 }
@@ -42,6 +57,7 @@ impl AppState {
         Arc::new(Self {
             is_visible: AtomicBool::new(false),
             is_connected: AtomicBool::new(false),
+            is_launched: AtomicBool::new(false),
             players: ArcSwap::from_pointee(HashMap::new()),
             config: ArcSwap::from_pointee(Config::default()),
         })
